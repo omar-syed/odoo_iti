10c6c806e9a3160255bad0ee0058fbd64a6dd2d5
omar-syed <omarrrrr118@gmail.com>
2024-05-28 16:22:41 +0300

    Odoo Orm create write search browse unlink

--- mansoura/iti24/models/met_student.py
+++ mansoura/iti24/models/met_student.py
@@ -1,11 +1,12 @@
 from odoo import models,fields,api;
-
+from odoo.exceptions import UserError
 
 class ItiStudent(models.Model):
     _name = "met.student"
     # _log_access = False
     
     name = fields.Char(required=True)
+    email = fields.Char()
     birth_date = fields.Date()
     salary = fields.Float()
     address = fields.Text()
@@ -21,10 +22,68 @@ class ItiStudent(models.Model):
     track_capacity = fields.Integer(related='track_id.capacity')
     skills_ids = fields.Many2many('iti.skill')
     grade_ids = fields.One2many('student.skill.line','student_id')
+    state = fields.Selection(
+        [
+            ('applied','Applied'),
+            ('first','First Interview'),
+            ('second','Second Interview'),
+            ('passed','Passed'),
+            ('rejected','Rejected'),
+        ],default = 'applied'
+    )
+    
+    # @api.model
+    # def create(self,vals):
+    #     new_student = super().create(vals)
+    #     name_split = new_student.name.split()
+    #     new_student.email = f"{name_split[0][0]}{name_split[1]}@gmail.com"
+    #     return new_student
+    @api.model
+    def create(self,vals):
+        name_split = vals['name'].split()
+        vals['email'] = f"{name_split[0][0]}{name_split[1]}@gmail.com"
+        search_students = self.search([('email','=',vals['email'])],limit=5,offset=5)
+        track = self.env['iti.track'].browse(vals['track_id'])
+        if track.is_open is False:
+            raise UserError("Selected track is close")
+        if search_students:
+            raise UserError('Email aleardy exist')
+        return super().create(vals)
+    
+    def write(self,vals):
+        if 'name' in vals:
+            name_split = vals['name'].split()
+            vals['email'] = f"{name_split[0][0]}{name_split[1]}@gmail.com"
+        super().write(vals)
+    
+    def unlink(self):
+        for record in self :
+            if record.state in ['passed','rejected']:
+                raise UserError("you can't delete passed/rejected student")
+        super().unlink()
+            
+    
+    def change_state(self):
+        if self.state == 'applied':
+            self.state = 'first'
+        elif self.state == 'first':
+            self.state = 'second'
+        elif self.state in ['passed','rejected']:
+            self.state = 'applied'
+            
+    def set_passed(self):
+        self.state='passed'
+        
+    def set_rejected(self):
+        self.state='rejected'
+        
 
     @api.onchange("gender")
     def _on_change_gender(self):
         domain = {'track_id':[]}
+        if not self.gender:
+            self.gender = 'm'
+            return{}
         if self.gender == 'm':
             domain = {'track_id':[('is_open','=',True)]}
             self.salary = 10000

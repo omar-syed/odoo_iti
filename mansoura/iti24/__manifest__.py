797c05822f3cb0e9cbf95e2d291cc5ea4ae67ecd
omar-syed <omarrrrr118@gmail.com>
2024-05-29 22:31:18 +0300

    odoo security about models level

--- mansoura/iti24/__manifest__.py
+++ mansoura/iti24/__manifest__.py
@@ -3,9 +3,10 @@
     "author": "ITI OMAR",
     "description": "First module containing iti students and tracks",
     "data":[
-        
+        "security/iti24_security.xml",
+        "security/ir.model.access.csv",
         "views/iti_track_views.xml",
-        "views/met_student_views.xml"
+        "views/met_student_views.xml",
         
     ]
     

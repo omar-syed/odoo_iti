a61b4b5ff25dbd55da9e9a141c1ab21777e51a17
omar-syed <omarrrrr118@gmail.com>
2024-05-30 15:16:42 +0300

    odoo Qweb Reports

--- mansoura/iti24/__manifest__.py
+++ mansoura/iti24/__manifest__.py
@@ -3,6 +3,8 @@
     "author": "ITI OMAR",
     "description": "First module containing iti students and tracks",
     "data":[
+        "reports/iti_student_templates.xml",
+        "reports/iti_reports.xml",
         "security/iti24_security.xml",
         "security/ir.model.access.csv",
         "views/iti_track_views.xml",
